b85db94d6cfcf39eb07f13c0955d04709b010682
Krystian Kasprzyk <Krystian.Kasprzyk@microsoft.wsei.edu.pl>
2022-03-26 22:49:46 +0100

    Create a constructor for StandardGradeBook

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -7,9 +7,10 @@ namespace GradeBook.GradeBooks
 {
     public class StandardGradeBook : BaseGradeBook
     {
+       
         public StandardGradeBook(string name) : base(name)
         {
-
+            this.Type = GradeBookType.Standard;
 
         }
             
